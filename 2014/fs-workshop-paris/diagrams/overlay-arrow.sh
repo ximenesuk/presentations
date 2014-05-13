97d4376bed2baae89a6f1bda538cc51eea1ce33f
Colin Blackburn <colin@ximenes.org.uk>
2014-05-13 14:14:20 +0100

    Remove old image files

--- 2014/fs-workshop-paris/diagrams/overlay-arrow.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-pstopnm -xsize 3800 -stdout arrow.ps | pnmcrop >arrow.ppm
-<arrow.ppm ppmdist >arrow.pgm
-<to-client-v5.png pngtopnm | pnmcomp -xoff=1375 -yoff=725 -invert -alpha=arrow.pgm arrow.ppm | pnmtopng -compression 9 -transparent rgb:f/f/f >to-client-v5-rp.png
-rm arrow.p??
